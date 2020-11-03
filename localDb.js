482e6dc9fb54478703c1f2fcb9801be66556a1a9
Samprit <gupta.samprit@gmail.com>
2020-11-03 19:48:50 +0530

    n

--- localDb.js
+++ localDb.js
@@ -15,6 +15,30 @@ const db= {
       chunks: ['o','r'],
       phones: ['AH','R']
     },
+    a: {
+      chunks: ['a'],
+      phones: ['AA']
+    },
+    from: {
+      chunks: ['f','r','o','m'],
+      phones: ['PHO','R','OM']
+    },
+    them: {
+      chunks: ['t','h','e','m'],
+      phones: ['DH','AH','EM']
+    },
+    has: {
+      chunks: ['h','a','s'],
+      phones: ['HA','S']
+    },
+    have: {
+      chunks: ['h','a','v','e'],
+      phones: ['HA', 'V','E']
+    },
+    had: {
+      chunks: ['h','a','d'],
+      phones: ['HA','D']
+    }
     
 }
 
